ea9df1ae109d36a388ee09ab0ae2f3f45376c91f
Suman <desuman18@gmail.com>
2021-02-14 19:38:03 +0530

    Todo API

--- main.py
+++ main.py
@@ -39,29 +39,78 @@
 
 # template ....
 
-from fastapi import FastAPI, Request, Form
-from fastapi.templating import Jinja2Templates
+# from fastapi import FastAPI, Request, Form
+# from fastapi.templating import Jinja2Templates
 
 
-app = FastAPI()
-templates = Jinja2Templates(directory="templates/")
+# app = FastAPI()
+# templates = Jinja2Templates(directory="templates/")
+
+# @app.get('/')
+# def read_form():
+#     return 'hello world'
+
+
+# @app.get("/form")
+# def form_post(request: Request):
+#     result = "Type a number"
+#     return templates.TemplateResponse('index.html', context={'request': request, 'result': result})
+
+
+# @app.post("/form")
+# def form_post(request: Request, num: int = Form(...)):
+#     result = num
+#     return templates.TemplateResponse('index.html', context={'request': request, 'result': result})
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Optional
+
+class Todo(BaseModel):
+    name: str
+    time: str
+    description: str
+
+app = FastAPI(title="Todo API")
+
+tasks = []
 
 @app.get('/')
-def read_form():
-    return 'hello world'
+async def home():
+    return "Hello welcome to the Todo api"
 
+@app.get('/todo/')
+async def all_todo():
+    return tasks
 
-@app.get("/form")
-def form_post(request: Request):
-    result = "Type a number"
-    return templates.TemplateResponse('index.html', context={'request': request, 'result': result})
+@app.post('/todo/')
+async def add_todo(todo: Todo):
+    tasks.append(todo)
+    return todo
 
+@app.get('/todo/{id}')
+async def get_todo(id: int):
+    try:
+        return tasks[id]
+    except:
+        raise HTTPException(status_code=404, detail='id is invalid')
 
-@app.post("/form")
-def form_post(request: Request, num: int = Form(...)):
-    result = num
-    return templates.TemplateResponse('index.html', context={'request': request, 'result': result})
+@app.put('/todo/{id}')
+async def get_todo(id: int, updated_todo: Todo):
+    try:
+        tasks[id] = updated_todo
+        return updated_todo
+    except:
+        raise HTTPException(status_code=404, detail='id is invalid')
 
+@app.delete('/todo/{id}')
+async def get_todo(id: int):
+    try:
+        todo = tasks[id]
+        tasks.pop(id)
+        return todo
+    except:
+        raise HTTPException(status_code=404, detail='id is invalid')
 
 
 
